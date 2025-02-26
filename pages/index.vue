fa9bc17532d92dbb2898b593da7d286e17f3c8bf
AmanYize <amanexo19@gmail.com>
2025-02-26 14:32:21 +0300

    added components routes and fetch api's

---
+++ pages/index.vue
@@ -0,0 +1,25 @@
+<template>
+  <div>
+    <h2>Home</h2>
+    <p>
+      Lorem ipsum dolor sit amet consectetur adipisicing elit. Autem consequatur
+      culpa esse eveniet. Nemo, consectetur eum esse nihil obcaecati inventore?
+    </p>
+    <p>
+      Lorem ipsum dolor sit amet consectetur adipisicing elit. Autem consequatur
+      culpa esse eveniet. Nemo, consectetur eum esse nihil obcaecati inventore?
+    </p>
+  </div>
+</template>
+
+<script setup></script>
+
+<style scoped>
+h2 {
+  margin-bottom: 20px;
+  font-size: 36px;
+}
+p {
+  margin: 20px 0;
+}
+</style>
